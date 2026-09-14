f71f65443c7512d255db6a586a81ca42702f8086
videha-ejournal <ggajendra@gmail.com>
2026-09-14 17:57:13 +0530

    Remove obsolete DOM compatibility shim

--- app/dom-parentnode-compat.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-
-declare global {
-  // Browser Elements implement ParentNode (querySelector/querySelectorAll and child traversal).
-  // The Vinext/worker type environment currently omits that DOM inheritance edge.
-  interface Element extends ParentNode {}
-}
